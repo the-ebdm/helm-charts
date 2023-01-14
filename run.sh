151b9b342f1fcd62fd3e7b6309809ba3faac3cb2
Eric Muir <eric.muir@xdesign.com>
2023-01-14 13:06:03 +0000

    Update charts and index

--- run.sh
+++ run.sh
@@ -13,4 +13,13 @@ git add -A
 git commit -m "Update charts and index"
 git push
 
+CURRENT_GEN=$(cat index.yaml | yq -r '.generated')
 LAST_GEN=$(curl -s https://ebdmuir.github.io/helm-charts/index.yaml | yq -r '.generated')
+
+echo $LAST_GEN
+
+while [ "$CURRENT_GEN" != "$LAST_GEN" ]; do
+  echo "Waiting for index to update"
+  sleep 5
+  LAST_GEN=$(curl -s https://ebdmuir.github.io/helm-charts/index.yaml | yq -r '.generated')
+done
